86d0929a1b1731702e6dbfa964951b41dd2801f3
Keisz Dominik <keiszdominik@Keisz-MacBook-Pro.local>
2021-07-12 20:33:03 +0200

    ACM and Route 53 tf setup

---
+++ backend/env/prod/route53.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_record" "root-a" {
+  zone_id = var.domain_name
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www-a" {
+  zone_id = var.bucket_name
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
