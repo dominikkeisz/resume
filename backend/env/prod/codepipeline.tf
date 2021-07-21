674d59d019edfe429c6ac15aea6f35b1d3d997ee
Dominik Keisz <keisz.d@gmail.com>
2021-07-21 15:18:36 +0200

    First version of CodePipeline

--- backend/env/prod/codepipeline.tf
+++ backend/env/prod/codepipeline.tf
@@ -1,3 +1,55 @@
+resource "aws_codepipeline" "codepipeline" {
+  name     = "resume-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+
+    encryption_key {
+      id   = aws_kms_alias.resumekmskey.arn
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.github.arn
+        FullRepositoryId = "dominikkeisz/resume"
+        BranchName       = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "S3"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        BucketName = "${var.domain_name}"
+        Extract = "true"
+      }
+    }
+  }
+}
+
 resource "aws_codestarconnections_connection" "github" {
   name          = "github-connection"
   provider_type = "GitHub"
